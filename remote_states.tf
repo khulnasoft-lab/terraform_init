f44cb0e6cdaa000feeba81839aca1521b6cc5fd5
Md Sulaiman <51925710+sulaiman-coder@users.noreply.github.com>
2023-09-02 14:21:40 +0600

    Create remote_states.tf

---
+++ remote_states.tf
@@ -0,0 +1,11 @@
+data "terraform_remote_state" "cool_assessment_terraform" {
+  backend = "s3"
+  config = {
+    encrypt = true
+    bucket  = "khulnasoft-cool-terraform-state"
+    profile = "read_cool_assessment_terraform_state"
+    region  = "us-east-1"
+    key     = "cool-assessment-terraform/terraform.tfstate"
+  }
+  workspace = local.assessment_workspace_name
+}
